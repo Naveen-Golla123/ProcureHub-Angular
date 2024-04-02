375a193805dbd605b216304a234db65b4f4ee646
Golla <naveeng@csu.fullerton.edu>
2024-04-02 10:31:47 -0700

    statble buyer page

---
+++ src/buyer/buyer-live/chat-box/message-panel/message-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-panel',
+  templateUrl: './message-panel.component.html',
+  styleUrls: ['./message-panel.component.scss']
+})
+export class MessagePanelComponent {
+
+}

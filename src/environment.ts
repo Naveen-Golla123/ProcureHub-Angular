ae0c42f0300d24cd016ed9a871298ce579d5936b
Naveen Kumar Golla <naveeng@csu.fullerton.edu>
2024-04-16 10:13:02 -0700

    URL fixed

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production : false,
-    // baseUrl: "https://webapplication120240414185844.azurewebsites.net/"
-    baseUrl: "https://localhost:44375/"
+    baseUrl: "https://webapplication120240414185844.azurewebsites.net/"
+    //baseUrl: "https://localhost:44375/"
 }
